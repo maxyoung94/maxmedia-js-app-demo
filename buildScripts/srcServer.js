0d82a2d6bbd7c3d935c381cfd8b2176479fdbaf7
MaxMedia <maxyoung1994@gmail.com>
2017-08-07 14:56:39 -0600

    - created mock api with hardcoded json - generated table using that date - using whatwg-fetch

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -21,6 +21,15 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
+app.get('/users', function(req, res) {
+    // hard coding for simplicity, pretend this hits a real database
+    res.json([
+        {"id": 1, "firstName": "Max", "lastName": "Young", "email":"maxyoung1994@gmail.com"},
+        {"id": 2, "firstName": "Bob", "lastName": "Young", "email":"bob@gmail.com"},
+        {"id": 3, "firstName": "Tim", "lastName": "Young", "email":"tim@gmail.com"}
+    ]);
+});
+
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 // import css files here for production only
 import './index.css';
+import {getUsers} from './api/userApi';
+
+// populate table of users via api call
+getUsers().then(result => {
+    let usersBody = '';
+
+    result.forEach(user => {
+        usersBody += `<tr>
+            <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+            <td>${user.id}</td>
+            <td>${user.firstName}</td>
+            <td>${user.lastName}</td>
+            <td>${user.email}</td>
+            </tr>`
+    });
+
+    global.document.getElementById('users').innerHTML = usersBody;
+});
